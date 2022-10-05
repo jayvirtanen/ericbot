2966986085b78ade1374802edb04b1a2f9fe6c65
Jay Virtanen <83300606+jayvirtanen@users.noreply.github.com>
2022-10-05 19:32:44 -0400

    Update ericbot.py

--- ericbot.py
+++ ericbot.py
@@ -1,33 +1,36 @@
 import time
-import os
 import discord
+from discord.ext import commands
 from datetime import datetime
+
 
 intents = discord.Intents.all()
 intents.members = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='.', intents=intents)
 
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-@client.event
+@bot.command(name='..')
+async def yoooo(ctx):
+    response = 'https://www.youtube.com/watch?v=MxV2u46ER9M'
+    await ctx.send(response)
+
+@bot.event
 async def on_message(message):
+    await bot.process_commands(message)
     now = datetime.now()
     sender = str(message.author)
     t = round(time.time())
     print(sender)
     print(t)
     hour = int(now.hour)%12
-    if sender == 'Target username' and t%2:
+    if sender == 'HotSaucePacket#3198' and t%2:
         await message.channel.send("Eric, it's " + str(hour).zfill(2) + ":" + str(now.minute).zfill(2))
-
-@bot.command(name='ERIC')
-async def yoooo(ctx):
-    response = 'https://www.youtube.com/watch?v=MxV2u46ER9M'
-    await ctx.send(response)
 
 bot.run('TOKEN')
 
