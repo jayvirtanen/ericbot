24912c932114243c1d1e3546b0f836ce69f3050c
Jay Virtanen <83300606+jayvirtanen@users.noreply.github.com>
2022-10-05 16:27:59 -0400

    Update ericbot.py

--- ericbot.py
+++ ericbot.py
@@ -5,10 +5,12 @@ from dotenv import load_dotenv
 from datetime import datetime
 
 load_dotenv()
-TOKEN = 'TOKEN'
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.all()
+intents.members = True
 
-bot = commands.Bot(command_prefix='!')
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
+
 
 @client.event
 async def on_ready():
@@ -22,16 +24,12 @@ async def on_message(message):
     print(sender)
     print(t)
     hour = int(now.hour)%12
-    if sender == 'TARGET#user' and t%2:
+    if sender == 'Target username' and t%2:
         await message.channel.send("Eric, it's " + str(hour).zfill(2) + ":" + str(now.minute).zfill(2))
-
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
 
 @bot.command(name='ERIC')
 async def yoooo(ctx):
-    response = https://www.youtube.com/watch?v=MxV2u46ER9M
+    response = 'https://www.youtube.com/watch?v=MxV2u46ER9M'
     await ctx.send(response)
 
 client.run('TOKEN')
