74220803cb8a06150f2422de9063f93ec3c2be51
jani.j.k.virtanen <jani.j.k.virtanen@gmail.com>
2022-10-05 20:22:02 -0400

    added yo functionality

--- ericbot.py
+++ ericbot.py
@@ -1,15 +1,14 @@
 import time
 import os
-import random
-import random
 import discord
 from dotenv import load_dotenv
-
 from datetime import datetime
 
 load_dotenv()
-TOKEN = ''
+TOKEN = 'TOKEN'
 client = discord.Client(intents=discord.Intents.default())
+
+bot = commands.Bot(command_prefix='!')
 
 @client.event
 async def on_ready():
@@ -18,7 +17,6 @@ async def on_ready():
 @client.event
 async def on_message(message):
     now = datetime.now()
-    sleep = random.randrange(3600)
     sender = str(message.author)
     t = round(time.time())
     print(sender)
@@ -27,4 +25,15 @@ async def on_message(message):
     if sender == 'TARGET#user' and t%2:
         await message.channel.send("Eric, it's " + str(hour).zfill(2) + ":" + str(now.minute).zfill(2))
 
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.command(name='ERIC')
+async def yoooo(ctx):
+    response = https://www.youtube.com/watch?v=MxV2u46ER9M
+    await ctx.send(response)
+
 client.run('TOKEN')
+bot.run('TOKEN')
+
